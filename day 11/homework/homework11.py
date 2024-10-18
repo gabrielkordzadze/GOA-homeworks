e450d5c646ff7e8c40e4c850e29531fcf40644dd
gabrielkordzadze <gabrielkordzadze2011@gmail.com>
2024-10-18 16:09:48 +0400

    second commit

---
+++ day 11/homework/homework11.py
@@ -0,0 +1,24 @@
+#2)შექმენით 8 ცვლადი სადაც შეინახავთ ყველა შესაძლო ლოგიკური ოპერატორის 
+# მაგალითს (4 ცალი or / 4 ცალი and)
+
+operator = "True or True"
+Operator = "True or False"
+OPERATOR =  "False or True"
+OPerator = "False or False"
+
+OPEratot = "True and True"
+OPERator = "True nd False"
+OPERAtor = "False and True"
+OPERATor = "False and False"
+
+#3)მომხმარებელს შემოატანინეთ მისი სახელი, შემოატანინეთ მისი ასაკი,
+#  ასევე სიმაღლე და შეამოწმეთ თუ მომხმარებლის ასაკი მეტია 18 და მისი
+#  სახელი უდრის თქვენს სახელს და ასევე მისი სიმაღლე მეტია 1.80-ზე
+ 
+name = input("enter your name:")
+print(name == "gabrieli")
+
+age = int(input("enter your age:"))
+print(age > 18)
+
+height = int(input("enter your height:"))
